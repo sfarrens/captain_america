1c97d2004a8fb9d8c7ffe8371195f23b41102ca5
Tobias Russell <tobias.russell95@gmail.com>
2025-09-09 17:02:03 +0100

    Edit test_matter

--- test_glass_cannon/test_matter.py
+++ test_glass_cannon/test_matter.py
@@ -4,14 +4,14 @@ import numpy as np
 #doesnt work - not a package
 from glass_cannon.matter import run_ln_fields
 
-def test_lognormal_fields(shells=glass.linear_windows([0., 0.04720758, 0.09549701, 0.14497038, 0.19573705, 0.24791426,
+def test_lognormal_fields():
+
+    shells=glass.linear_windows([0., 0.04720758, 0.09549701, 0.14497038, 0.19573705, 0.24791426,
     0.30162785, 0.35701293, 0.41421482, 0.47338997, 0.53470702, 0.59834801,
-    0.66450976, 0.73340537, 0.80526592, 0.88034248, 0.95890821, 1.04126096])):
+    0.66450976, 0.73340537, 0.80526592, 0.88034248, 0.95890821, 1.04126096])
 
     #expected output
-    expected_fields = glass.lognormal_fields(glass.linear_windows([0., 0.04720758, 0.09549701, 0.14497038, 0.19573705, 0.24791426,
-    0.30162785, 0.35701293, 0.41421482, 0.47338997, 0.53470702, 0.59834801,
-    0.66450976, 0.73340537, 0.80526592, 0.88034248, 0.95890821, 1.04126096]))
+    expected_fields = glass.lognormal_fields(shells)
 
     fields = run_ln_fields(shells)
 
