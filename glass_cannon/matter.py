c0da9dea9ca7005e85a38091e3314c68b1635771
Tobias Russell <tobias.russell95@gmail.com>
2025-09-10 14:01:42 +0100

    Add unit test discretized_cls

--- glass_cannon/matter.py
+++ glass_cannon/matter.py
@@ -1,8 +1,21 @@
 import glass
+import camb
 
 def run_ln_fields(shells):
 
     fields = glass.lognormal_fields(shells)
 
     return fields
+
+def run_discretized_cls(cls, nside, lmax):
+
+    # apply discretisation to the full set of spectra:
+    # - HEALPix pixel window function (`nside=nside`)
+    # - maximum angular mode number (`lmax=lmax`)
+    # - number of correlated shells (`ncorr=3`)
+    cls = glass.discretized_cls(cls, nside=nside, lmax=lmax, ncorr=3)
+
+    return cls
+
+
 
--- test_glass_cannon/test_matter.py
+++ test_glass_cannon/test_matter.py
@@ -1,10 +1,11 @@
 import glass
 import numpy as np
+import camb
 
-#doesnt work - not a package
 from glass_cannon.matter import run_ln_fields
+from glass_cannon.matter import run_discretized_cls
 
-def test_lognormal_fields():
+def test_run_ln_fields():
 
     shells=glass.linear_windows([0., 0.04720758, 0.09549701, 0.14497038, 0.19573705, 0.24791426,
     0.30162785, 0.35701293, 0.41421482, 0.47338997, 0.53470702, 0.59834801,
@@ -18,7 +19,30 @@ def test_lognormal_fields():
     #eg assert if x = x or if x is true or false
     assert expected_fields == fields
 
-    
+def test_run_discretized_cls():
+
+    nside = lmax = 128
+
+    h = 0.7
+    Oc = 0.25
+    Ob = 0.05
+
+    pars = camb.set_params(
+    H0=100 * h,
+    omch2=Oc * h**2,
+    ombh2=Ob * h**2,
+    NonLinear=camb.model.NonLinear_both,
+    )
+
+    shells=glass.linear_windows([0., 0.04720758, 0.09549701, 0.14497038, 0.19573705, 0.24791426,
+    0.30162785, 0.35701293, 0.41421482, 0.47338997, 0.53470702, 0.59834801,
+    0.66450976, 0.73340537, 0.80526592, 0.88034248, 0.95890821, 1.04126096])
+
+    prev_cls = glass.ext.camb.matter_cls(pars, lmax, shells)
+
+    cls = run_discretized_cls(cls, nside, lmax)
+
+    assert 0 #prev_cls == cls
 
 
 
