4d845b9e805adfa95ade7cbb9f81f3d4f489f29f
Jade Piat <jade.piat99@gmail.com>
2025-09-10 16:36:34 +0100

    created source code directory

---
+++ src/glass_cannon/.ipynb_checkpoints/Cls-checkpoint.py
@@ -0,0 +1,15 @@
+# use the CAMB cosmology that generated the matter power spectra
+import camb
+from cosmology.compat.camb import Cosmology
+
+# almost all GLASS functionality is available from the `glass` namespace
+import glass
+import glass.ext.camb
+
+def angular_power_spectrum(pars,lmax,zb):
+    
+    # linear radial window functions
+    shells = glass.linear_windows(zb)
+    
+    # compute the angular matter power spectra of the shells with CAMB
+    return glass.ext.camb.matter_cls(pars, lmax, shells)
